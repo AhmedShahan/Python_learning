694e2444636c3f6f6a1d6f11f0935b51ea52d40a
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-04 16:17:44 +0600

    special syntex in variable assignment

--- 9_variable_advance.py
+++ 9_variable_advance.py
@@ -12,3 +12,25 @@ x="Shahan"
 print(type(x))
 x=3.14416
 print(type(x))
+
+
+### special syntex in variable assignemnt
+
+# In a sinle line declearation
+x="Shahan"; y=1001; z=3.1416
+print(x)
+print(y)
+print(z)
+
+# same value multiple variable
+a=b=c=10
+print(a)
+print(b)
+print(c)
+
+
+# *** Most useful variable assign
+a,b,c=5,6,7
+print(a)
+print(b)
+print(c)
