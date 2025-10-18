58a768de4d37d7444fcf3ff58fb3b1d8ed535579
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-18 12:19:27 +0600

    all daTATYPE TO bool

---
+++ 11_Datatype.py
@@ -0,0 +1,17 @@
+int_value=10
+int_value2=0
+float_value=3.1416
+string_value="This is String"
+bool_value=True
+list_value=[1,2,3,4,5]
+tuple_value=("Apple","Banala","apple","mango")
+set_value=("Apple","Banala","apple","mango")
+
+
+print(bool(int_value))
+print(bool(int_value2))
+print(bool(float_value))
+print(bool(string_value))
+print(bool(list_value))
+print(bool(tuple_value))
+print(bool(set_value))
