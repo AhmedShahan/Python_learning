c6d99346581421d8e958c2aa04e4a82269b0e5fb
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-19 17:58:55 +0600

    method 22 formating string

--- 12_formatting.py
+++ 12_formatting.py
@@ -9,4 +9,11 @@ name="Shahan"
 age=27
 cgpa=3.8
 
-print("I am %s and I am %d years old also my CGPA is %f"%(age, name, cgpa))
+# print("I am %s and I am %d years old also my CGPA is %f"%(age, name, cgpa))
+
+## Method 2
+
+print("I am {} and I am {} years old also my CGPA is {}".format(name, age, cgpa))
+print("I am {1} and I am {0} years old also my CGPA is {2}".format(age, name, cgpa))
+print("I am {n} and I am {a} years old also my CGPA is {cg}".format(a=age, n=name, cg=cgpa))
+
