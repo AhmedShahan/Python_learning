6fe14b879c1706588cb6ac5b1bef55ee4cbe36cb
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-19 10:29:49 +0600

    bool to str,float,int

--- 11_Datatype.py
+++ 11_Datatype.py
@@ -1,17 +1,43 @@
-int_value=10
-int_value2=0
-float_value=3.1416
-string_value="This is String"
-bool_value=True
-list_value=[1,2,3,4,5]
-tuple_value=("Apple","Banala","apple","mango")
-set_value=("Apple","Banala","apple","mango")
+############ ANY TO Boolean ##############
+
+# int_value=10
+# int_value2=0
+# float_value=3.1416
+# string_value="This is String"
+# string_value2=""
+# bool_value=True
+# list_value=[1,2,3,4,5]
+# list_value2=[]
+# tuple_value=("Apple","Banala","apple","mango")
+# tuple_value2=()
+# set_value=("Apple","Banala","apple","mango")
 
 
-print(bool(int_value))
-print(bool(int_value2))
-print(bool(float_value))
-print(bool(string_value))
-print(bool(list_value))
-print(bool(tuple_value))
-print(bool(set_value))
+# print(bool(int_value))
+# print(bool(int_value2))
+# print(bool(float_value))
+# print(bool(string_value))
+# print(bool(string_value2))
+# print(bool(list_value))
+# print(bool(list_value2))
+# print(bool(tuple_value))
+# print(bool(tuple_value2))
+# print(bool(set_value))
+########################################
+
+
+### Boolean --> int, float, string
+
+
+
+bool_value1=True
+bool_value2=False
+
+print(int(bool_value1))
+print(int(bool_value2))
+print(float(bool_value1))
+print(float(bool_value2))
+print(str(bool_value1))
+print(str(bool_value2))
+print((bool_value2))
+
