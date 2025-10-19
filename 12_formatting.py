d6957a979ad8ae10f5d58e73016bd11086fe25b0
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-19 17:41:34 +0600

    python formating method 1

---
+++ 12_formatting.py
@@ -0,0 +1,12 @@
+## Method 1: % (Ol style as like c)
+'''
+%s---> ffor string
+%f---> for float
+%dd--> for decimal or integer
+'''
+
+name="Shahan"
+age=27
+cgpa=3.8
+
+print("I am %s and I am %d years old also my CGPA is %f"%(age, name, cgpa))
