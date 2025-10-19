514f9ca3b24822ecf02cda093361aff18ad8db1b
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-19 12:34:36 +0600

    string to int, bool, float

--- 11_Datatype.py
+++ 11_Datatype.py
@@ -28,16 +28,59 @@
 
 ### Boolean --> int, float, string
 
+# bool_value1=True
+# bool_value2=False
 
+# print(int(bool_value1))
+# print(int(bool_value2))
+# print(float(bool_value1))
+# print(float(bool_value2))
+# print(str(bool_value1))
+# print(str(bool_value2))
+# print((bool_value2))
 
-bool_value1=True
-bool_value2=False
+############################################
 
-print(int(bool_value1))
-print(int(bool_value2))
-print(float(bool_value1))
-print(float(bool_value2))
-print(str(bool_value1))
-print(str(bool_value2))
-print((bool_value2))
+## aNY ----> str
+# int_value=10
+# int_value2=0
+# float_value=3.1416
+# string_value="This is String"
+# string_value2=""
+# bool_value=True
+# list_value=[1,2,3,4,5]
+# list_value2=[]
+# tuple_value=("Apple","Banala","apple","mango")
+# tuple_value2=()
+# set_value=("Apple","Banala","apple","mango")
 
+# print(str(int_value))
+# print(str(int_value2))
+# print(str(float_value))
+# print(str(string_value))
+# print(str(string_value2))
+# print(str(list_value))
+# print(str(list_value2))
+# print(str(tuple_value))
+# print(str(tuple_value2))
+# print(str(set_value))
+
+#####################################################
+
+## str ---> int, float, bool
+
+string_1="This is String"
+string_2="10"
+string_3="10.14"
+string_4="True"
+
+list_value=str([1,2,3,4,5])
+
+tuple_value=str(("Apple","Banala","apple","mango"))
+set_value=str(("Apple","Banala","apple","mango"))
+
+print(int(string_2))
+print(float(string_3))
+print(bool(string_4))
+print(bool(string_4))
+print(list(tuple_value))
