4c631978ee70af9ab992bba502daef9bb5fd3bb7
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-22 11:53:09 +0600

    user input multiple in one prompt

---
+++ 13_user_input.py
@@ -0,0 +1,27 @@
+# name=input("Enter your name: ")
+# print(type(name))
+# '''
+# Output: 
+# Enter your name: shahan
+# <class 'str'>
+# '''
+##############################################################
+# age=input("Enter your age: ")
+# print(type(age))
+# '''
+# Enter your age: 30
+# <class 'str'>
+# '''
+##############################################################
+
+# age=int(input("Enter your age: "))
+# print(type(age))
+# '''
+# Enter your age: 30
+# <class 'int'>
+# '''
+##############################################################
+
+
+a,b=input("Enter Numbers: ").split()
+print(a,b)
