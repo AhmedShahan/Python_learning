4068bd91253fdf65054c73da170b097d4f021f9e
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-29 11:01:56 +0600

    string literal in python

---
+++ 18_python_literal_string.py
@@ -0,0 +1,32 @@
+string1="This is string"
+string2='This is string'
+char="c"
+
+multi_line_str1="""
+This is multi line string. 
+I am learning python
+"""
+
+multi_line_str2='''
+This is multi line string. 
+I am learning python
+'''
+
+# print("\U0001F600")  # 😀 (Grinning face)
+# print("\U0001F602")  # 😂 (Face with tears of joy)
+# print("\U0001F525")  # 🔥 (Fire)
+# print("\U0001F9E0")  # 🧠 (Brain)
+# print("\U0001F64F")  # 🙏 (Folded hands)
+
+
+unicode_string=u"\U0001F600\U0001F602\U0001F525\U0001F9E0\U0001F64F"
+
+raw_string=r"This is \n for new line in coding"
+
+
+print(string1)
+print(string2)
+print(multi_line_str1)
+print(multi_line_str2)
+print(unicode_string)
+print(raw_string)
