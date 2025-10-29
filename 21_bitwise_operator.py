a4632f9363f32d5753d91964031ee653bfb71be0
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-29 17:58:52 +0600

    bitwise operator

---
+++ 21_bitwise_operator.py
@@ -0,0 +1,30 @@
+x=2
+y=3
+
+# print(x & y)
+'''
+x--> 2---> 10
+y--> 3---> 11
+----------------
+           10
+AND logic on (0,1) (1,1) ==> 10  so output will be 2
+
+'''
+
+# print(x | y)
+'''
+x--> 2---> 10
+y--> 3---> 11
+----------------
+           11
+OR logic on (0,1) (1,1) ==> 11  so output will be 3
+'''
+
+
+# print(not(x))
+'''
+x---> 2 so boolean (x) -- True    so not(true) ===> False
+'''
+
+print(x^y)
+
