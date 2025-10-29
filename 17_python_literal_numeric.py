6c632991f34dfe61f828ab4d3d20038f7201655b
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-29 10:56:33 +0600

    python numerical literal

---
+++ 17_python_literal_numeric.py
@@ -0,0 +1,78 @@
+# a=100
+# b=0b1010
+# c=0o310
+# d=0x12c
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+#### Decimal ---> Bin, Octal, Hex
+# a = 1234
+# a_bin = bin(a)
+# a_oct=oct(a)
+# a_hex=hex(a)
+# print(a_bin)
+# print(a_oct)
+# print(a_hex)
+
+'''
+0b10011010010
+0o2322
+0x4d2
+'''
+
+ ## Without prefix
+# print(a_bin[2:])
+# print(a_oct[2:])
+# print(a_hex[2:])
+
+'''
+10011010010
+2322
+4d2
+'''
+
+##### Format function
+# a = 1234
+# a_bin = format(a, 'b')
+# a_oct = format(a, 'o')
+# a_hex = format(a, 'x')
+# print(a_bin) 
+# print(a_oct) 
+# print(a_hex) 
+'''
+10011010010
+2322
+4d2
+
+'''
+
+# a = 1234
+# print(f"{a:b}")
+# print(f"{a:o}")
+# print(f"{a:x}")
+
+
+
+# float_1=1.5
+# float_2=1.5e2
+# float_3=1.5e-3
+
+# print(float_1)
+# print(float_2)
+# print(float_3)
+
+
+
+
+x=3.14j
+
+print(x)
+print(x.imag)
+print(x.real)
+
+
+
+
