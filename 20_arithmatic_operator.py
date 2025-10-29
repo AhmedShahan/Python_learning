d0766cb6f60d04be219698e83d2aab6d1f3b8cd1
AhmedShahan <shahan.ahmed001@gmail.com>
2025-10-29 15:26:05 +0600

    arithmatic operator

---
+++ 20_arithmatic_operator.py
@@ -0,0 +1,10 @@
+a=10
+b=7
+
+print(f"Sum of {a} + {b}= {a+b}")
+print(f"Sub of {a} - {b}= {a-b}")
+print(f"Multiplication of {a} * {b}= {a*b}")
+print(f"Division of {a} / {b}= {a/b}")
+print(f"Divisor of {a} / {b}= {a//b}")
+print(f"Remainder of {a} / {b}= {a%b}")
+print(f"Power of {a} ^ {b}= {a**b}")
